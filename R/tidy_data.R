a97984dea17c45cd0fc3c6e6db01b4727f3943a4
wandadk <wandadekeersmaecker@gmail.com>
2021-01-11 15:33:26 +0100

    functions to tidy data GEE

--- R/tidy_data.R
+++ R/tidy_data.R
@@ -1,51 +1,58 @@
-library(tidyverse)
-# setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-input_folder <- "/home/wanda/Documents/data/upscaleRecovery/test"
-output_folder <- "/home/wanda/Documents/data/upscaleRecovery/test"
-file_list <- "test2.csv"#list.files(path=input_folder,pattern="test2.csv")
-
-
-for (file in file_list){
-  a <- str_split(tools::file_path_sans_ext(file),"_")%>%unlist
-  tmpf <- tempfile()
-  x <- readLines(file.path(input_folder,file))
-  y <-  substr(x,2,str_length(x)-1)  
+#' Tidy GEE data
+#'
+#' @param x input data
+#' @param col_names column names associated with the input data
+#'
+#' @return
+#' @export
+#' @import tidyverse
+#'
+tidy_data <- function(x, col_names){
+  # a <- str_split(tools::file_path_sans_ext(file),"_")%>%unlist
+  # remove the '[' at the beginning and end of the file
+  y <-  substr(x,2,str_length(x)-1)
+  # replace the "]" by "["
   z <- gsub( "]", "[", y )
   z <- z[-1]
+  # write to temporary file
+  tmpf <- tempfile()
   cat(z, file=tmpf, sep="\n")
-  col_names <- c("id","ecoReg","lossyr","coords","img_names","series")
-  # 
+
+  # load data again, to get rid of the "[" seperator and assign column names
   dat_orig <- read_csv(tmpf, quote="[",
-                        col_names=col_names)
-  # count number of images per series
+                       col_names=col_names)
+  # count number of images per time series
   dat_format <- dat_orig%>%
     mutate(nimg=str_count(img_names,",")+1)
 
-  extract_date <- function(x){
-    # stl <- str_count(x)
-    # substr(x,20,27)
-    sp <- strsplit(x,'_')
-    L <- lapply(sp, function(x) {
-      if(length(x) > 1){x[length(x)-1]
-        } else{x[1]}} )
-    # sp[[1]][]
-    unlist(L)
-  }
-  
-  
-  
-  # x <- dat_format$img_names[18]
-  
+  # get the maximum number of observations per time series
   max_img <- max(dat_format$nimg)
-  nvars_img=sprintf("p__%04i",seq(from=1,to=max_img))
-  nvars_1=sprintf("series__%04i",seq(from=1,to=max_img))
-  
+  nvars_img=sprintf("p__%04i",seq(from=1,to=max_img))# column names for the dates
+  nvars_1=sprintf("series__%04i",seq(from=1,to=max_img))# column names for the time series observations
+
+  # separate the string of image names and NBR series into columns, extract date from image name, and replace 'None' by 'NA'
   dat_format_img <- dat_format%>%
     separate(img_names,into=nvars_img,sep=", ",convert=TRUE)%>%
     separate(series,into=nvars_1,sep=", ",convert=TRUE)%>%
     mutate_at(vars(starts_with("p__")),extract_date)%>%
     mutate_all(~str_replace(.,"None","NA"))
-  
-  write_csv(dat_format_img, file.path(output_folder,paste0('tidy_',file)))
-  
+
+  return(dat_format_img)
+}
+
+#' Extract date from image name
+#'
+#' Helper function for tidy_data
+#'
+#' @param x image name
+#'
+#' @return date
+#' @export
+#'
+extract_date <- function(x){
+  sp <- strsplit(x,'_')
+  L <- lapply(sp, function(x) {
+    if(length(x) > 1){x[length(x)-1]
+    } else{x[1]}} )
+  unlist(L)
 }

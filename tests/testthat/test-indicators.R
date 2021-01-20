e3d6e207d64ef4188bf0671dc7f41e5abb121f7d
wandadk <wandadekeersmaecker@gmail.com>
2021-01-20 15:33:59 +0100

    add tests R80p and RRI

--- tests/testthat/test-indicators.R
+++ tests/testthat/test-indicators.R
@@ -52,40 +52,40 @@ test_that("Out of bounds", {
                "*bounds*")
 })
 
-# context("Recovery indicator: R80p")
+context("Recovery indicator: R80p")
 
-# test_that("Fully recovered time series", {
-#   # Generate a time series
-#   ts <- seq(-2, 10, by = 0.1)
-#   ys <- exponential(ts, pert = -2, offset = 1, thalf = 0.5)
-#
-#   expect_equal(r80p(ts, ys), 1 / 0.8, tolerance = 1e-2)
-# })
-#
-# test_that("NAs are tolerated", {
-#   # Generate a time series
-#   ts <- seq(-2, 10, by = 0.1)
-#   ts[5] <- NA # Introduce NA
-#   ys <- exponential(ts, pert = -2, offset = 1, thalf = 0.5)
-#
-#   expect_equal(r80p(ts, ys), 1 / 0.8, tolerance = 1e-2)
-# })
-#
-# context("Recovery indicator: RRI")
+test_that("Fully recovered time series", {
+  # Generate a time series
+  ts <- seq(-2, 10, by = 0.1)
+  ys <- c(rep(1,20),seq(-1,1,by=0.2),rep(1,90))#exponential(ts, pert = -2, offset = 1, thalf = 0.5)
 
-# test_that("Fully recovered time series", {
-#   # Generate a time series
-#   ts <- seq(-2, 10, by = 0.1) # as a vector of times
-#   ys <- exponential(ts, pert = -2, offset = 1, thalf = 0.25) # plus a vector of values
-#
-#   expect_equal(rri(ts, ys), 1, tolerance = 1e-4)
-# })
-#
-# test_that("NAs are tolerated", {
-#   # Generate a time series
-#   ts <- seq(-2, 10, by = 0.1)
-#   ts[5] <- NA # Introduce NA
-#   ys <- exponential(ts, pert = -2, offset = 1, thalf = 0.25) # plus a vector of values
-#
-#   expect_equal(rri(ts, ys), 1, tolerance = 1e-2)
-# })
+  expect_equal(r80p(ts, ys), 1 / 0.8, tolerance = 1e-2)
+})
+
+test_that("NAs are tolerated", {
+  # Generate a time series
+  ts <- seq(-2, 10, by = 0.1)
+  ts[5] <- NA # Introduce NA
+  ys <- c(rep(1,20),seq(-1,1,by=0.2),rep(1,90))#exponential(ts, pert = -2, offset = 1, thalf = 0.5)
+
+  expect_equal(r80p(ts, ys), 1 / 0.8, tolerance = 1e-2)
+})
+
+context("Recovery indicator: RRI")
+
+test_that("Fully recovered time series", {
+  # Generate a time series
+  ts <- seq(-2, 10, by = 0.1) # as a vector of times
+  ys <- c(rep(1,20),seq(-1,1,by=0.4),rep(1,95))#exponential(ts, pert = -2, offset = 1, thalf = 0.25) # plus a vector of values
+
+  expect_equal(rri(ts, ys), 1, tolerance = 1e-4)
+})
+
+test_that("NAs are tolerated", {
+  # Generate a time series
+  ts <- seq(-2, 10, by = 0.1)
+  ts[5] <- NA # Introduce NA
+  ys <- c(rep(1,20),seq(-1,1,by=0.4),rep(1,95))#exponential(ts, pert = -2, offset = 1, thalf = 0.25) # plus a vector of values
+
+  expect_equal(rri(ts, ys), 1, tolerance = 1e-2)
+})

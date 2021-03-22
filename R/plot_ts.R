ba84c4e2e6592d65781910c32c9465d5d8dddf84
wandadk <wandadekeersmaecker@gmail.com>
2021-03-22 17:46:59 +0100

    improve plot function, increase flexibility

--- R/plot_ts.R
+++ R/plot_ts.R
@@ -15,14 +15,16 @@
 #' @return plot
 #' @export
 #'
-plot_aux <- function(dts, obsi,main, lossdt_start,lossdt_end,lossdt_start1,lossdt_end1,
-                     trend,met_rri,met_r80p,met_yryr){
-  plot(dts,obsi, xlab = 'Time', ylab = 'NBR [-]', main = paste0(main))
+plot_aux <- function(dts, obsi,main, lossdt_start,lossdt_end,predt_start,distdt_end,postdt_start,postdt_end,
+                     trend,met_rri,met_r80p,met_yryr,...){
+  plot(dts,obsi, xlab = 'Time', ylab = 'NBR [-]', main = paste0(main),...)
   lines(dts[!is.na(obsi)],obsi[!is.na(obsi)])
   abline(v = lossdt_start, col = 'red')
   abline(v = lossdt_end, col = 'red')
-  abline(v = lossdt_start1, col = 'blue')
-  abline(v = lossdt_end1, col = 'blue')
+  abline(v = distdt_end, col = 'gray')
+  abline(v = predt_start, col = 'gray')
+  abline(v = postdt_start, col = 'gray')
+  abline(v = postdt_end, col = 'gray')
   if(length(trend)>1){
     lines(dts,trend, col = 'green')
   }
